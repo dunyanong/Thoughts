a69251d828366ed99f9d3636fb82a787b9049a1d
dun_yan_ <dunyanong2004@gmail.com>
2022-11-15 21:51:05 +0800

    first commit

---
+++ api/index.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('../models/blog');
+const { render } = require('ejs');
+const { v4 } = require('uuid');
+
+// express app
+const app = express();
+
+// connect to MongoDB
+const dbURI = 'mongodb+srv://Sony:WTL6183@cluster0.9xi6mad.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then((request) => {
+        // listen to request
+        app.listen(3000);
+        console.log('Connected to DB');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.get('/api', (req, res) => {
+    const path = `/api/item/${v4()}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+    });
+    
+    app.get('/api/item/:slug', (req, res) => {
+    const { slug } = req.params;
+    res.end(`Item: ${slug}`);
+    });
+    
+    module.exports = app;
+
+// To create view engine:
+app.set('view engine', 'ejs');
+
+// static file of style.css with the foldername of public
+app.use(express.static('public'));
+
+// Creating an express server
+app.get('/', (req, res) => {
+    res.redirect('/blogs')
+    
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'Rules'});
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'New Confession'});
+});
+
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('index', {title: 'All Blogs', blogs: result}) 
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+});;
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+  
+    blog.save()
+      .then(result => {
+        res.redirect('/blogs');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+        .then(result => {
+            res.render('details', { blog: result, title: 'Blog Details' });
+        })
+        .catch(err => {
+            console.log(err);
+        })
+});
+
+app.use((req, res) => {
+    res.status(404).render('404', { title: 'Page not found'});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
