2e8815c860776047d8210b724c11e5c47084cf07
Dun Yan <97597966+dunyanong@users.noreply.github.com>
2022-11-17 15:37:22 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,6 +8,19 @@ const { v4 } = require('uuid');
 // express app
 const app = express();
 
+// for vercel
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
 // connect to MongoDB
 const dbURI = 'mongodb+srv://Sony:WTL6183@cluster0.9xi6mad.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
@@ -90,6 +103,8 @@ app.get('/blogs/:id', (req, res) => {
 app.use((req, res) => {
     res.status(404).render('404', { title: 'Page not found'});
 });
+
+module.exports = app;
 
 
 
